5cebe4e19377b61ecfb6f9178c4d1b0c4bc15999
herve.marchal <herve.marchal@hotmail.fr>
2017-02-17 19:04:07 +0100

    SPAWN0 is created

---
+++ Ghosty/src/blabla/Spawn0.java
@@ -0,0 +1,13 @@
+package blabla;
+
+public class Spawn0 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Je suis le mechant Spawn Zero !!!");
+
+	}
+
+}
---
+++ Ghosty/src/blabla/Spawn2.java
@@ -0,0 +1,13 @@
+package blabla;
+
+public class Spawn2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Je suis le mechant Spawn2 !!!");
+
+	}
+
+}
