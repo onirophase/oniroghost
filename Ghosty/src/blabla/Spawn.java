23958da45023bd7a36c0ed83206056c98ddbba2c
herve.marchal <herve.marchal@hotmail.fr>
2017-02-17 18:56:32 +0100

    Spawn ressucité

---
+++ Ghosty/src/blabla/Spawn.java
@@ -0,0 +1,13 @@
+package blabla;
+
+public class Spawn {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Je suis Caper le gentil fantome !!!");
+
+	}
+
+}
